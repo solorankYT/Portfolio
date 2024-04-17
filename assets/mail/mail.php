8fbe7051cff4171db2d58438b3be543f9c2bedb5
solorankYT <43119617+solorankYT@users.noreply.github.com>
2024-04-17 15:13:12 +0800

    Update mail.php

--- assets/mail/mail.php
+++ assets/mail/mail.php
@@ -15,7 +15,7 @@ Message: ".$message."
 
 ";
 
-mail ("example@gmail.com" , "New Message", $email_message);
+mail ("markfuna6@gmail.com" , "New Message", $email_message);
 header("location: ../../mail-success.html");
 ?>
 
