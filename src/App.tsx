8996a3ebddcbcb93f837552001a7c3a9c73b6b6f
solorankYT <43119617+solorankYT@users.noreply.github.com>
2026-02-17 03:13:19 +0800

    Navbar Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,27 @@ import NavBar from "./components/navBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectPage from "./ProjectPage";
-import ScrollToTop from "./components/ScrollToTop";
+// import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 dark:bg-gray-950 flex flex-col">
-     
-      <NavBar />
-        <ScrollToTop/>
-      <main className="flex-grow">
-        <Routes>
-        
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projectpage" element={<ProjectPage />} />
-        </Routes>
-      </main>
+    <div className="min-h-screen bg-gray-900 dark:bg-gray-950 flex">
       
+      {/* <ScrollToTop /> */}
+
+      <NavBar />
+
+        <main className="flex-1 ml-64  p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projectpage" element={<ProjectPage />} />
+          </Routes>
+        </main>
+
+
+
     </div>
+
   );
 }
